e150a113ee3eb187dd21a54ed7e640ab3517e684
nathaniel <nagoldfarb@wpi.edu>
2020-10-03 14:59:59 -0400

    generated the walking pickel file
    generated the picke file

--- stair_climbing_paper/TranJoint.py
+++ stair_climbing_paper/TranJoint.py
@@ -76,16 +76,16 @@ if __name__ == "__main__":
     #                            ["Subject00", "Subject01", "Subject02", 'Subject03', "subject04", "Subject05",
     #                             "Subject06", "Subject07", "Subject08", "Subject10"])
 
-    angles = plot_joint_angles(["/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_00/subject_00 walk_00.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_01/subject_01_walk_00.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_02/subject_02_walk_00.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_03/subject_03_walk_00.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_04/subject_04_walk_00.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_05/subject_05_walk_00.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_06/subject_06 walk_00.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_07/subject_07 walk_01.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_08/subject_08_walking_01.csv",
-                       "/home/nathaniel/AIM_GaitData/Gaiting_stairs/subject_10/subject_10 walk_02.csv",
+    angles = plot_joint_angles(["/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_00/subject_00 walk_00.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_01/subject_01_walk_00.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_02/subject_02_walk_00.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_03/subject_03_walk_00.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_04/subject_04_walk_00.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_05/subject_05_walk_00.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_06/subject_06 walk_00.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_07/subject_07 walk_01.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_08/subject_08_walking_01.csv",
+                       "/home/nathanielgoldfarb/AIM_GaitData/Gaiting_stairs/subject_10/subject_10 walk_02.csv",
                        ],
                       [0, 0, 1, 0, 0, 0, 0, 0, 0, 2],
                       ["R", "R", "R", "R", "R", "R", "R", "R", "R", "R"],
@@ -105,8 +105,8 @@ if __name__ == "__main__":
     #                   ["Subject00", "Subject01", "Subject02", 'Subject03', "subject04", "Subject05", "Subject06",
     #                    "Subject07", "Subject08", "Subject10"])
 
-    traj = [angles["hip"]]
-    trainer = TPGMMTrainer.TPGMMTrainer(demo=traj, file_name="leg", n_rf=15, dt=0.01, reg=[1e-8, 1e-8, 1e-8], poly_degree=[15, 15, 15])
+    traj = [angles["hip"], angles["knee"], angles["ankle"]  ]
+    trainer = TPGMMTrainer.TPGMMTrainer(demo=traj, file_name="leg", n_rf=15, dt=0.01, reg=[1e-5, 1e-7, 1e-7], poly_degree=[20, 20, 20])
     trainer.train()
     runner = TPGMMRunner.TPGMMRunner("leg.pickle")
     path = np.array(runner.run())
